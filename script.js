12afc5d5582b735c1304f246c85549a12ce175ad
AlaaAj2001 <114425831+AlaaAj2001@users.noreply.github.com>
2024-03-03 11:35:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+const gifWindow = document.getElementById('gifWindow');
+const triangle = document.getElementById('triangle');
+const openGIFWindowBtn = document.getElementById('openGIFWindowBtn');
+const gifContainer = document.getElementById('gifContainer');
+const searchInput = document.querySelector('.gif-window input[type=text]');
+const loadingSpinner = document.getElementById('loadingSpinner');
+var page = 0;
+const API_KEY = 'zu9ERYECic4uci9TYt3n7QqnkWPpi4Pf';
+
+async function fetchGIFs(offset = 0) {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=10&rating=g&offset=${offset}`);
+    const data = await response.json();
+    const gifURLs = data.data.map(gif => gif.images.original.url);
+    return gifURLs;
+  } catch (error) {
+    console.error('Error fetching GIFs:', error);
+  }
+}
+
+async function fetchGIFsFromSearch(offset = 0, searchQuery) {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=${API_KEY}&limit=10&rating=g&offset=${offset}`);
+    const data = await response.json();
+    const gifURLs = data.data.map(gif => gif.images.original.url);
+    return gifURLs;
+  } catch (error) {
+    console.error('Error fetching GIFs:', error);
+  }
+}
+
+async function generateGIFs(offset = 0) {
+  const gifURLs = await fetchGIFs(offset);
+  gifURLs.forEach(url => {
+    const gifItem = document.createElement('li');
+    gifItem.classList.add('gif-item');
+    const img = document.createElement('img');
+    img.src = url;
+    gifItem.appendChild(img);
+    gifContainer.appendChild(gifItem);
+
+    gifItem.addEventListener('click', () => {
+      const div = document.createElement('div');
+      div.style.position = 'fixed';
+      div.style.width = '100%';
+      div.style.height = '100%';
+      div.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+      div.style.display = 'flex';
+      div.style.justifyContent = 'center';
+      div.style.alignItems = 'center';
+    
+      const selectedImg = document.createElement('img');
+      selectedImg.src = url;
+      selectedImg.style.width = 'auto';
+      selectedImg.style.height = 'auto';
+    
+      div.appendChild(selectedImg);
+      document.body.appendChild(div);
+    
+      div.addEventListener('click', () => {
+        document.body.removeChild(div);
+      });
+    });
+  });
+}
+
+async function generateGIFsFromSearch(offset = 0, searchQuery) {
+  const gifURLs = await fetchGIFsFromSearch(offset, searchQuery);
+  gifURLs.forEach(url => {
+    const gifItem = document.createElement('li');
+    gifItem.classList.add('gif-item');
+    const img = document.createElement('img');
+    img.src = url;
+    gifItem.appendChild(img);
+    gifContainer.appendChild(gifItem);
+
+    gifItem.addEventListener('click', () => {
+      const div = document.createElement('div');
+      div.style.position = 'fixed';
+      div.style.width = '100%';
+      div.style.height = '100%';
+      div.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+      div.style.display = 'flex';
+      div.style.justifyContent = 'center';
+      div.style.alignItems = 'center';
+    
+      const selectedImg = document.createElement('img');
+      selectedImg.src = url;
+      selectedImg.style.width = 'auto';
+      selectedImg.style.height = 'auto';
+    
+      div.appendChild(selectedImg);
+      document.body.appendChild(div);
+    
+      div.addEventListener('click', () => {
+        document.body.removeChild(div);
+      });
+    });
+  });
+}
+
+openGIFWindowBtn.addEventListener('click', async () => {
+  loadingSpinner.style.display = 'block';
+  const searchQuery = searchInput.value.trim();
+  gifContainer.innerHTML = ''; 
+  page = 0;
+  if(searchQuery !== '') { 
+    await generateGIFsFromSearch(0, searchQuery);
+  } else {
+    await generateGIFs(0);  
+  } 
+  loadingSpinner.style.display = 'none';
+  gifWindow.style.display = 'block';
+  triangle.style.display = 'block';
+
+});
+
+searchInput.addEventListener('input', async () => {
+  loadingSpinner.style.display = 'block';
+  const searchQuery = searchInput.value.trim();
+  gifContainer.innerHTML = '';
+  page = 0;
+  if (searchQuery !== '') { 
+    await generateGIFsFromSearch(0, searchQuery);
+  } else {
+    await generateGIFs(0);  
+  } 
+  loadingSpinner.style.display = 'none';
+  gifWindow.style.display = 'block';
+  triangle.style.display = 'block';
+});
+
+gifWindow.addEventListener('scroll', async () => {
+  const { scrollTop, scrollHeight, clientHeight } = gifWindow;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    const searchQuery = searchInput.value.trim();
+    if(searchQuery !== '') { 
+      await generateGIFsFromSearch((page + 1) * 10, searchQuery);
+    } else {
+      await generateGIFs((page + 1) * 10);  
+    } 
+    page++;
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (!gifWindow.contains(event.target) && event.target !== openGIFWindowBtn) {
+    gifWindow.style.display = 'none';
+    triangle.style.display = 'none';
+  }
+});
